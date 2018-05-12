204d2832bcef0ea8fec150d752869f85d99ea65f
Vincent Ambo <tazjin@gmail.com>
2018-05-12 14:32:49 +0200

    tests(command): Refactor tests to be compatible with new types

--- tests/command_test.rs
+++ tests/command_test.rs
@@ -4,40 +4,43 @@ mod circuit_breaker {
 
     use crius::command::Config;
     use crius::command::Command;
-    use crius::command::RunnableCommand;
-    use crius::command::error::RejectError;
+    use crius::error::CriusError;
     use std::error::Error;
     use std::fmt::Display;
     use std::fmt;
     use std::{thread, time};
 
     #[derive(PartialEq, Eq, Copy, Clone, Debug)]
-    struct TestError {}
+    enum TestError { Internal, External }
     impl Display for TestError {
         fn fmt(&self, f: &mut fmt::Formatter) ->  fmt::Result {
              write!(f, "An error happened")
          }
     }
+
     impl Error for TestError {
         fn description(&self) -> &str {
             return "An error happened"
         }
     }
-    unsafe impl Send for TestError {}
-    unsafe impl Sync for TestError {}
+
+    impl From<CriusError> for TestError {
+        fn from(_: CriusError) -> Self { TestError::External }
+    }
+
+    type TestCommand<I, O> = Command<I, O, TestError,
+                                     fn(I) -> Result<O, TestError>,
+                                     fn(TestError) -> O>;
 
     #[test]
     fn runs_command() {
-        let rx = Command::define(|_| {
-                return Ok(5)
-            }).create().run(());
-
+        let rx = TestCommand::<(), u8>::define(|_| Ok(5)).create().run(());
         assert_eq!(5, rx.recv().unwrap().unwrap());
     }
 
     #[test]
     fn runs_command_multiple_times() {
-        let mut cmd = Command::define(|_| {
+        let mut cmd = TestCommand::<(), u8>::define(|_| {
             return Ok(5)
         }).create();
 
@@ -49,32 +52,33 @@ mod circuit_breaker {
 
     #[test]
     fn runs_command_with_param() {
-        let rx = Command::define(|param| {
-            return Ok(param)
-        }).create().run(5);
+        let rx = TestCommand::<u8, u8>::define(|param| Ok(param))
+            .create().run(5);
 
         assert_eq!(5, rx.recv().unwrap().unwrap());
     }
 
     #[test]
     fn rejects_command_if_circuit_open() {
-        let mut cmd: RunnableCommand<(), (), _, _> = Command::define(|_| {
-            return Err(Box::new(TestError {}));
+        let mut cmd = TestCommand::<(), ()>::define(|_| {
+            Err(TestError::Internal)
         }).config(*Config::new().error_threshold(5)).create();
 
         for _ in 0..5 {
             let rx = cmd.run(());
-            assert_eq!(true, rx.recv().unwrap().unwrap_err().is::<TestError>()); // Fallback by returned error
+            let err = rx.recv().unwrap().unwrap_err();
+            assert_eq!(TestError::Internal, err); // Fallback by returned error
         }
 
         let rx = cmd.run(());
-        assert_eq!(true, rx.recv().unwrap().unwrap_err().is::<RejectError>()); // Fallback by reject error
+        let err = rx.recv().unwrap().unwrap_err();
+        assert_eq!(TestError::External, err); // Fallback by reject error
     }
 
     #[test]
     fn returns_fallback_if_err_result_returned() {
         let mut cmd = Command::define_with_fallback(|_| {
-            return Err(Box::new(TestError {}))
+            return Err(TestError::Internal)
         }, |_| {
             return 5;
         }).create();
@@ -86,7 +90,7 @@ mod circuit_breaker {
     #[test]
     fn returns_fallback_if_circuit_open() {
         let mut cmd = Command::define_with_fallback(|_| {
-            return Err(Box::new(TestError {}))
+            return Err(TestError::Internal)
         }, |_| {
             return 5;
         }).config(*Config::new().error_threshold(5)).create();
@@ -102,7 +106,7 @@ mod circuit_breaker {
 
     #[test]
     fn handles_lots_of_calls() {
-        let mut cmd = Command::define(|_| {
+        let mut cmd = TestCommand::<(), u8>::define(|_| {
             let ten_millis = time::Duration::from_millis(10);
             thread::sleep(ten_millis);
 
